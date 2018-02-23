a5776039f443464126cae8e1dd83142dc0190dc7
Levon Grigoryan <levon@bandwango.com>
2018-02-23 04:51:21 +0400

    fixes index.js

--- index.js
+++ index.js
@@ -51,63 +51,46 @@
         if (validateContactForm()) {
             res.sendStatus(200);
 
-            sendmail({
-                logger: {
-                    debug: console.log,
-                    info: console.info,
-                    warn: console.warn,
-                    error: console.error
-                },
-                from: "no-reply@bitcoinlightning.co.uk",
-                to: "levon1.grigoryan@gmail.com",
-                subject: "Contact Request",
-                html: "Hello World!",
-            }, function (err, reply) {
-                console.log(err && err.stack);
-                console.dir(reply);
+            // Generate test SMTP service account from ethereal.email
+            // Only needed if you don't have a real mail account for testing
+            nodemailer.createTestAccount((err, account) => {
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                    host: 'smtp.ethereal.email',
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                        user: account.user, // generated ethereal user
+                        pass: account.pass // generated ethereal password
+                    }
+                });
+
+
+                let mailOptions = {
+                    from: userData.username,
+                    to: userData.email,
+                    subject: "Contact Request",
+                    html: "<p>" + userData.message + "</p>"
+                };
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: %s', info.messageId);
+                    // Preview only available when sending through an Ethereal account
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                });
+
             });
         }
         else {
-            console.log(false);
+            return alert("form is not valid");
         }
 
         next();
     });
 
-    // MAIL SEND
-
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    nodemailer.createTestAccount((err, account) => {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass // generated ethereal password
-            }
-        });
-
-
-        let mailOptions = {
-            from: userData.username,
-            to: userData.email,
-            subject: "Contact Request",
-            html: "<p>" + userData.message + "</p>"
-        };
-
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
-
-    });
 
     function validateContactForm() {
         //email

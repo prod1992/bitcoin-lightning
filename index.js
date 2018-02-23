dd0675db29ead4c060636c4b0c1e9b4c286c1553
Levon Grigoryan <levon@bandwango.com>
2018-02-23 05:07:28 +0400

    fixes index.js

--- index.js
+++ index.js
@@ -47,41 +47,33 @@
         userData.email = takeInfo.email;
         userData.phone = takeInfo.phone;
         userData.message = takeInfo.message;
-        let phoneCheck = /^\d{10}$/;
+
         if (validateContactForm()) {
             res.sendStatus(200);
 
-            // Generate test SMTP service account from ethereal.email
-            // Only needed if you don't have a real mail account for testing
-            nodemailer.createTestAccount((err, account) => {
-                // create reusable transporter object using the default SMTP transport
-                let transporter = nodemailer.createTransport({
-                    host: 'smtp.ethereal.email',
-                    port: 587,
-                    secure: false, // true for 465, false for other ports
-                    auth: {
-                        user: account.user, // generated ethereal user
-                        pass: account.pass // generated ethereal password
-                    }
-                });
+            let transporter = nodemailer.createTransport({
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: "hhb6e72okmkggopp@ethereal.email", // generated ethereal user
+                    pass: "ASppCxYCXJSu4JuuYA" // generated ethereal password
+                }
+            });
+            let mailOptions = {
+                from: userData.username + "<" + userData.email + ">",
+                to: "levon1.grigoryan@gmail.com",
+                subject: "Contact Request",
+                html: "<p>" + userData.message + "</p>"
+            };
 
-
-                let mailOptions = {
-                    from: userData.username,
-                    to: "levon1.grigoryan@gmail.com",
-                    subject: "Contact Request",
-                    html: "<p>" + userData.message + "</p>"
-                };
-
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        return console.log(error);
-                    }
-                    console.log('Message sent: %s', info.messageId);
-                    // Preview only available when sending through an Ethereal account
-                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-                });
-
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
             });
         }
         else {
